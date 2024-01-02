c419b99c1eec97bb7ad942395e420c2202f5f23f
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2024-01-02 21:50:02 +0000

    bind img data

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product.module';
 
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
+
 export class CatalogComponent {
+  product!: IProduct;
+  
+  constructor(){
+    this.product=  {
+      id: 1,
+      description:
+        "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+      name: "Large Cyclops",
+      imageName: "head-big-eye.png",
+      category: "Heads",
+      price: 1220.5,
+      discount: 0.2,
+    }
+  }
 
 }
