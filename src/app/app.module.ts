83464b0d0cf735723f071d30f32ac05772d346b0
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2024-01-02 14:54:58 +0000

    add catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
