49aaac06a8f17ad2ac6ba26844e674438d7cc9ee
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2024-01-04 00:19:02 +0000

    talk to parent

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.module';
-import productsData from "../../../_course-resources/catalog/products-data"
+import productsData from '../../../_course-resources/catalog/products-data';
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
-
 export class CatalogComponent {
-  products: IProduct[]= productsData;
-  filterLabel:string = ""
-  cart: IProduct[]=[]
+  products: IProduct[] = productsData;
+  filterLabel: string = '';
+  cart: IProduct[] = [];
   // constructor(){
   //   this.products=  productsData
   // }
-  
-  getFilteredProducts= ()=>{
-    return this.filterLabel===""?this.products:this.products.filter(product=>product?.category===this.filterLabel)
+
+  getFilteredProducts = () => {
+    return this.filterLabel === ''
+      ? this.products
+      : this.products.filter(
+          (product) => product?.category === this.filterLabel
+        );
+  };
+  addToCart(product: IProduct) {
+    this.cart.push(product);
+    console.log(`i add ${product?.name} to cart`);
   }
-
-
 }
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,24 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.module';
 @Component({
   selector: 'bot-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
-  
- @Input()
+  @Input()
   product!: IProduct;
-  
-  cart: IProduct[]=[]
+  @Output() buy = new EventEmitter();
 
-  getImageUrl=(product:IProduct)=>{
-    console.log("what is product",product)
-    return "assets/images/robot-parts/"+ product?.imageName 
-  }
+  getImageUrl = (product: IProduct) => {
+    return 'assets/images/robot-parts/' + product?.imageName;
+  };
 
-  addToCart(product:IProduct){
-    this.cart.push(product)
-    console.log(`i add ${product?.name} to cart`)
+  buyButtonClicked() {
+    this.buy.emit();
   }
 }
