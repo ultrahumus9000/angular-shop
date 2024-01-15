c660d9e381c6b97f69789c2e2697a063aca38ef3
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2024-01-15 17:28:00 +0000

    set up cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
+    CartComponent,
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
