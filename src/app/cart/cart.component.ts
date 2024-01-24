6e6ad5e87db7cfce426a24de2f66869c31c8501d
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2024-01-24 23:49:02 +0000

    fix data structure

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,9 +9,28 @@ import { IProduct } from '../catalog/product.module';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-  cartTotal: any = 0;
-  cartItems: any;
-  constructor(private cartSvc: CartService) {}
+  cart: IProduct[] = [];
+  constructor(private cartService: CartService) {}
   getImageUrl = getImageUrl;
-  removeFromCart(product: IProduct) {}
+
+  ngOnInit() {
+    this.cartService.getCart().subscribe({
+      next: (cart) => (this.cart = cart),
+    });
+  }
+  get cartItems() {
+    return this.cart;
+  }
+
+  get cartTotal() {
+    return this.cart.reduce((prev, next) => {
+      let discount =
+        next?.discount && next.discount > 0 ? 1 - next.discount : 1;
+      return prev + (next?.price ?? 0) * discount;
+    }, 0);
+  }
+
+  removeFromCart(product: IProduct) {
+    this.cartService.remove(product);
+  }
 }
--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -1,48 +1,54 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from '../catalog/product.module';
-import { ILineItem } from '../catalog/ILineItem.module';
 import { HttpClient } from '@angular/common/http';
 import { getImageUrl } from '../utils';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  cart: ILineItem[] = [];
+  private cart: BehaviorSubject<IProduct[]> = new BehaviorSubject<IProduct[]>(
+    []
+  );
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.http
+      .get<IProduct[]>('/api/cart')
+      .subscribe((cartFromServer) => this.cart.next(cartFromServer));
+  }
 
   getImageUrl = getImageUrl;
 
+  getCart(): Observable<IProduct[]> {
+    return this.cart.asObservable();
+  }
+
+  // getTotalPrice() {
+  //   return Math.round(
+  //     this.cart.reduce((prev, cur) => {
+  //       return (
+  //         prev +
+  //         cur.qty *
+  //           ((cur.product?.price ?? 0) * (1 - (cur?.product?.discount ?? 0)))
+  //       );
+  //     }, 0)
+  //   );
+  // }
+
   add(product: IProduct) {
-    let lineItem = this.findLineItem(product);
-    if (lineItem !== undefined) {
-      lineItem.qty++;
-    } else {
-      lineItem = { product, qty: 1 };
-      this.cart.push(lineItem);
-    }
-    this.http.post('/api/cart', this.cart).subscribe((cart) => {
-      console.log('this is cart', cart);
-
-      console.log(`i add ${product?.name} to cart`);
+    const newCart = [...this.cart.getValue(), product];
+    this.cart.next(newCart);
+    this.http.post('/api/cart', newCart).subscribe(() => {
+      console.log('added ' + product?.name + ' to cart!');
     });
-
-    console.log(`total price is ${this.getTotalPrice()}`);
-  }
-  getTotalPrice() {
-    return Math.round(
-      this.cart.reduce((prev, cur) => {
-        return (
-          prev +
-          cur.qty *
-            ((cur.product?.price ?? 0) * (1 - (cur?.product?.discount ?? 0)))
-        );
-      }, 0)
-    );
   }
 
-  findLineItem(product: IProduct) {
-    return this.cart.find((li) => li.product?.id === product?.id);
+  remove(product: IProduct) {
+    let newCart = this.cart.getValue().filter((i) => i?.id !== product?.id);
+    this.cart.next(newCart);
+    this.http.post('/api/cart', newCart).subscribe(() => {
+      console.log('removed ' + product?.name + ' from cart!');
+    });
   }
 }
